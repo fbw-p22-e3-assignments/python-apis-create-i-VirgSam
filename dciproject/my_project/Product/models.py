0208fec0475007ea928cca2d95e0ca77d2be072e
chris01 <chris_odum@yahoo.com>
2023-05-17 11:36:16 +0200

    Create Class Product model and update .gitignore

---
+++ dciproject/my_project/Product/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    
+
+# product_name (String)
+# product_id (ID)
+# product_category
+# product_description (String)
+# product_image_url (String)
+# product_price (Integer)
