de7e741e14ac58a8dce82b81a1753e81b6e4ce36
chris01 <chris_odum@yahoo.com>
2023-05-17 11:53:17 +0200

    Update Product class with fields

--- dciproject/my_project/Product/models.py
+++ dciproject/my_project/Product/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    
+   # product_id (ID) use builtin ID
+   product_name= models.CharField(max_length=30)
+   product_category = [] # might be a class
+   product_description = models.TextField(max_length=500)
+   product_image_url = models.models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=None)
+   product_price = models.DecimalField(max_digits=None,decimal_places=2)
 
-# product_name (String)
-# product_id (ID)
-# product_category
-# product_description (String)
-# product_image_url (String)
-# product_price (Integer)
+
+
