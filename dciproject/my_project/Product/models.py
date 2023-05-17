f88437a6fa7f39084df9cbdc2f90ea9366e1a7b5
chris01 <chris_odum@yahoo.com>
2023-05-17 11:58:41 +0200

    Create a Category Class

--- dciproject/my_project/Product/models.py
+++ dciproject/my_project/Product/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+   name = models.CharField(max_length=100)
+
 class Product(models.Model):
    # product_id (ID) use builtin ID
    product_name= models.CharField(max_length=30)
    product_category = [] # might be a class
    product_description = models.TextField(max_length=500)
    product_image_url = models.models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=None)
-   product_price = models.DecimalField(max_digits=None,decimal_places=2)
+   product_price = models.DecimalField(max_digits=6,decimal_places=2)
 
 
 
